fe69e41bd293ae4cb5e1b80300e79daa6e8f7540
246Sudankhanal <sudankhanal246@gmail.com>
2026-07-12 11:42:48 +1000

    setup Automataed github actions

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const app=express()
-const PORT=3000
+const PORT=5000
 app.use(express.json({urlencoded:true}))
 app.get('/app',(req,res)=>{
-    res.json({message:"deployed first message"})
+    res.json({message:"deployed first message then again"})
 })
 app.listen(PORT,()=>{
     console.log("app is listening at port:",PORT)
