2e311cbedde094730de70548d53fc5e55693af08
246Sudankhanal <sudankhanal246@gmail.com>
2026-07-12 15:14:50 +1000

    new with new route

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.use(express.json({urlencoded:true}))
 app.get('/app',(req,res)=>{
     res.json({message:"deployed first message then again form ci/cd "})
 })
+app.get('/app/page',(req,res)=>{
+    res.send(`<html>
+        <body>
+        <form>
+        <input type="text placeholder="your name">Name</input>
+        </form>
+        </html>`)
+
+
+})
 app.listen(PORT,()=>{
     console.log("app is listening at port:",PORT)
 })
