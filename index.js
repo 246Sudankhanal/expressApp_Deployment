d14ab4e850358abb116c20add29610f6416615fb
246Sudankhanal <sudankhanal246@gmail.com>
2026-07-12 13:30:53 +1000

    final afeter cicd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express()
 const PORT=5000
 app.use(express.json({urlencoded:true}))
 app.get('/app',(req,res)=>{
-    res.json({message:"deployed first message then again form ci/cd"})
+    res.json({message:"deployed first message then again form ci/cd "})
 })
 app.listen(PORT,()=>{
     console.log("app is listening at port:",PORT)
